f191de31a3d54ef55d71faafd94d18009f32150f
Iurii <iamyurkas@gmail.com>
2025-08-27 00:57:50 +0100

    feat: show add button on ingredient screens

--- app/(tabs)/ingredients/all.tsx
+++ app/(tabs)/ingredients/all.tsx
@@ -1,37 +1,15 @@
-import { StyleSheet, TouchableOpacity } from 'react-native';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { useRouter } from 'expo-router';
 
 import { ThemedView } from '@/components/ThemedView';
 import IngredientList from '@/components/IngredientList';
-// eslint-disable-next-line import/no-unresolved
-import { useTheme } from 'react-native-paper';
+import { AddButton } from '@/components/AddButton';
 
 export default function AllIngredientsScreen() {
   const router = useRouter();
-  const { colors } = useTheme();
   return (
     <ThemedView style={{ flex: 1 }}>
       <IngredientList />
-      <TouchableOpacity
-        style={[styles.fab, { backgroundColor: colors.primary }]}
-        onPress={() => router.push('/add-ingredient')}
-      >
-        <MaterialIcons name="add" size={24} color={colors.onPrimary} />
-      </TouchableOpacity>
+      <AddButton onPress={() => router.push('/add-ingredient')} />
     </ThemedView>
   );
 }
-
-const styles = StyleSheet.create({
-  fab: {
-    position: 'absolute',
-    bottom: 16,
-    right: 16,
-    width: 56,
-    height: 56,
-    borderRadius: 28,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
