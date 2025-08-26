b28764e1c6b21cbd10d90f55479c81fbf23ffd7e
Iurii <iamyurkas@gmail.com>
2025-08-27 00:15:15 +0100

    feat: add top general menu component

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,10 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
+import { Platform, View } from 'react-native';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 import { HapticTab } from '@/components/HapticTab';
+import GeneralMenu from '@/components/GeneralMenu';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
@@ -12,47 +13,50 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="cocktails"
-        options={{
-          title: 'Cocktails',
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons size={28} name="local-bar" color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="shaker"
-        options={{
-          title: 'Shaker',
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons size={28} name="science" color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="ingredients"
-        options={{
-          title: 'Ingredients',
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons size={28} name="shopping-cart" color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+    <View style={{ flex: 1 }}>
+      <GeneralMenu />
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+          headerShown: false,
+          tabBarButton: HapticTab,
+          tabBarBackground: TabBarBackground,
+          tabBarStyle: Platform.select({
+            ios: {
+              // Use a transparent background on iOS to show the blur effect
+              position: 'absolute',
+            },
+            default: {},
+          }),
+        }}>
+        <Tabs.Screen
+          name="cocktails"
+          options={{
+            title: 'Cocktails',
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons size={28} name="local-bar" color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="shaker"
+          options={{
+            title: 'Shaker',
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons size={28} name="science" color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="ingredients"
+          options={{
+            title: 'Ingredients',
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons size={28} name="shopping-cart" color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </View>
   );
 }
