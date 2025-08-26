902ac92181411b8f6b887b1dbcc9d1908d851dad
Iurii <iamyurkas@gmail.com>
2025-08-27 00:32:57 +0100

    Remove redundant search bar in ingredient tabs

--- app/(tabs)/ingredients/_layout.tsx
+++ app/(tabs)/ingredients/_layout.tsx
@@ -3,7 +3,6 @@ import { createMaterialTopTabNavigator } from '@react-navigation/material-top-ta
 import React from 'react';
 import { View } from 'react-native';
 
-import SearchBar from '@/components/SearchBar';
 
 const { Navigator } = createMaterialTopTabNavigator();
 
@@ -12,7 +11,6 @@ export const Tabs = withLayoutContext(Navigator);
 export default function IngredientsLayout() {
   return (
     <View style={{ flex: 1 }}>
-      <SearchBar />
       <Tabs>
         <Tabs.Screen name="all" options={{ title: 'All' }} />
         <Tabs.Screen name="my" options={{ title: 'My' }} />
