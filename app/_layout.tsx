48f33cf0280f06ab766f7772d54dc346341d680e
Iurii <iamyurkas@gmail.com>
2025-08-27 00:23:52 +0100

    fix: use theme colors for status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,10 +5,13 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
+import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const backgroundColor =
+    colorScheme === 'dark' ? Colors.dark.background : Colors.light.background;
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -21,13 +24,16 @@ export default function RootLayout() {
   return (
     <SafeAreaProvider>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <SafeAreaView style={{ flex: 1, backgroundColor: 'transparent' }}>
+        <SafeAreaView style={{ flex: 1, backgroundColor }}>
           <Stack>
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
             <Stack.Screen name="+not-found" />
           </Stack>
         </SafeAreaView>
-        <StatusBar style="auto" />
+        <StatusBar
+          style={colorScheme === 'dark' ? 'light' : 'dark'}
+          backgroundColor={backgroundColor}
+        />
       </ThemeProvider>
     </SafeAreaProvider>
   );
