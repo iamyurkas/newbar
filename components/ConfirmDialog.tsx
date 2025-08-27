9bab9ccb26a4845f04cdfa118c1c72a101695925
Iurii <iamyurkas@gmail.com>
2025-08-27 15:12:27 +0100

    style: theme confirm dialog buttons

--- components/ConfirmDialog.tsx
+++ components/ConfirmDialog.tsx
@@ -24,7 +24,11 @@ export default function ConfirmDialog({
   const theme = useTheme();
   return (
     <Portal>
-      <Dialog visible={visible} onDismiss={onCancel} style={styles.dialog}>
+      <Dialog
+        visible={visible}
+        onDismiss={onCancel}
+        style={[styles.dialog, { backgroundColor: theme.colors.background }]}
+      >
         {title ? <Dialog.Title>{title}</Dialog.Title> : null}
         {message ? (
           <Dialog.Content>
@@ -33,11 +37,23 @@ export default function ConfirmDialog({
         ) : null}
         <Dialog.Actions>
           {cancelLabel && onCancel ? (
-            <Button textColor={theme.colors.onSurfaceVariant} onPress={onCancel}>
+            <Button
+              mode="outlined"
+              style={[styles.button, { borderColor: theme.colors.primary }]}
+              buttonColor={theme.colors.background}
+              textColor={theme.colors.primary}
+              onPress={onCancel}
+            >
               {cancelLabel}
             </Button>
           ) : null}
-          <Button textColor={theme.colors.primary} onPress={onConfirm}>
+          <Button
+            mode="contained"
+            style={styles.button}
+            buttonColor={theme.colors.primary}
+            textColor={theme.colors.onPrimary}
+            onPress={onConfirm}
+          >
             {confirmLabel}
           </Button>
         </Dialog.Actions>
@@ -49,6 +65,9 @@ export default function ConfirmDialog({
 const styles = StyleSheet.create({
   dialog: {
     borderRadius: 8,
+  },
+  button: {
+    borderRadius: 16,
   },
 });
 
